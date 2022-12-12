8e0892e44f1bc355bcc6e82bb2e0bb42a67c9247
LOGEDI <nelsonlogedi@gmail.com>
2022-12-12 18:24:41 +0000

    on progress

--- src/front/js/pages/Safari.jsx
+++ src/front/js/pages/Safari.jsx
@@ -3,7 +3,48 @@ import { Link } from 'react-router-dom'
 
 const Safari = () => {
   return (
-    <div>Safari</div>
+    <div>
+    <div class="jumbotron">
+     <div class="container">
+       <div class ="row">
+         <div class="col-sm 6 btn -holder">
+               <h1>SAFARIs</h1>
+               <p>
+               In the most private location away from the crowds inside the Masai Mara
+               </p>
+          </div>
+         <div class="col-sm 6 btn -holder">
+           <img src="https://thumbs.dreamstime.com/b/buffalo-isolated-black-background-buffalo-isolated-151574818.jpg" class="image-responsive"/>
+         </div>
+       </div>
+     </div>
+    </div>
+    <div class="jumbotron">
+     <div class="container">
+       <div class ="row">
+         <div class="col-sm 6 btn -holder">
+               <h1>FEEL OUR WILDLIFE</h1>
+               <p>
+                  Our privileged location inside the National Reserve together with our philosophy of tracking and searching for our own wildlife sightings far from the transited areas, makes the Enkewa safari experience amongst the best in the Masai Mara.
+
+                  We are located in superb big5 country with access to 4 different lion prides within minutes from camp as well as the best leopard territory inside the reserve. The Black rhino density in our area is the highest in the entire Maasai Mara-Serengeti Eco-System.
+
+                 For centuries the Maasai tribe has learned to coexist with its surroundings and its wildlife and our experienced and skilled Maasai spotters –trackers- will read all signs in order to find the best opportunities to observe the wildlife of the African Savannah.
+
+                 Our custom built 4×4 vehicles allow us to access the most remote places where we avoid the more crowded routes of the Mara as well as to enjoy privacy during our safaris.
+               </p>
+               <button type="button" class="btn btn-primary btn-lg">BOOK WITH US</button>
+          </div>
+         <div class="col-sm 6 btn -holder">
+           <img src="https://freedomdestinations.co.uk/wp-content/uploads/Elephant-Sillouette.jpg" class="image-responsive"/>
+         </div>
+       </div>
+     </div>
+    </div>
+  </div>
+
+
+     
   )
 }
 
