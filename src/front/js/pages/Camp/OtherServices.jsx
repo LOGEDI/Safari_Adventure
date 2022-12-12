ac3cf842895af3a6df627f1c0e4b96833e3f5038
DGgithubDG <thenumerousrandom@gmail.com>
2022-12-12 18:17:13 +0000

    changes

--- src/front/js/pages/Camp/OtherServices.jsx
+++ src/front/js/pages/Camp/OtherServices.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const OtherServices = () => {
   return (
-    <div>OtherServices</div>
-  )
-}
+    <div className="container">
+      {}
 
-export default OtherServices
+      <div className="page-header">
+        <div className="head-image">
+          <img
+            className="col-12 m-0 black-rock"
+            src="https://i.travelapi.com/hotels/1000000/530000/528800/528713/fc80b392_z.jpg"
+            alt=""
+          />
+        </div>
+        <div className="text-on-image">
+          <h2 className="page-title">OTHER SERVICES & AMENITIES</h2>
+          <span className="page-subtitle">
+          Enkewa Camp
+          </span>
+        </div>
+      </div>
+
+     
+     
+           
+
+
+
+      <div></div>
+    </div>
+  );
+};
+
+export default OtherServices;
