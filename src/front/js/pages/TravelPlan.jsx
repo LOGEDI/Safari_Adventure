5f106eeba6d089f786e106f2767dadfc2918c70e
DGgithubDG <thenumerousrandom@gmail.com>
2023-01-08 16:47:24 +0000

    post comment

--- src/front/js/pages/TravelPlan.jsx
+++ src/front/js/pages/TravelPlan.jsx
@@ -3,7 +3,28 @@ import { Link } from 'react-router-dom'
 
 const TravelPlan = () => {
   return (
-    <div>TravelPlan</div>
+    <div class="container mt-5">
+    <div class="d-flex justify-content-center row">
+        <div class="col-md-8">
+            <div class="d-flex flex-column comment-section">
+                <div class="">
+                    <div class="text-center">
+                        <div class="text-center">Leave A Review!</div>
+                    </div>
+                    <div class="mt-2">
+                        <p class="comment-text"></p>
+                    </div>
+                </div>
+                
+                </div>
+                <div class="bg-light p-2">
+                    <div class="d-flex flex-row align-items-start"><textarea class="form-control ml-1 shadow-none textarea"></textarea></div>
+                    <div class="mt-2 text-right"><button class="btn btn-primary btn-sm shadow-none" type="button">Post comment</button><button class="btn btn-outline-primary btn-sm ml-1 shadow-none" type="button">Cancel</button></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
   )
 }
 
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -330,6 +330,41 @@ const getState = ({ getStore, getActions, setStore }) => {
       },
 
       //-----------------------------------------------------------------------------------------------------------------------------
+      //											 COMMENT POST
+      //-----------------------------------------------------------------------------------------------------------------------------
+
+      // Use getActions to call a function within a fuction
+      exampleFunction: () => {
+        getActions().changeColor(0, "green");
+      },
+
+      getMessage: async () => {
+        try {
+          // fetching data from the backend
+          const resp = await fetch(process.env.BACKEND_URL + "/api/comment");
+          const data = await resp.json();
+          setStore({ message: data.message });
+          // don't forget to return something, that is how the async resolves
+          return data;
+        } catch (error) {
+          console.log("Error loading message from backend", error);
+        }
+      },
+      changeColor: (index, color) => {
+        //get the store
+        const store = getStore();
+
+        //we have to loop the entire demo array to look for the respective index
+        //and change its color
+        const demo = store.demo.map((elm, i) => {
+          if (i === index) elm.background = color;
+          return elm;
+        });
+
+        //reset the global store
+        setStore({ demo: demo });
+      },
+      //-----------------------------------------------------------------------------------------------------------------------------
       //								GET FAVORITES BY USER
       //-----------------------------------------------------------------------------------------------------------------------------
 
